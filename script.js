aa7d64f03347590a6f19a971ed4a313c99c2168e
Derrick Ampire <ampire90@gmail.com>
2023-05-08 14:56:25 +0300

    local storage and caching added

--- script.js
+++ script.js
@@ -5,6 +5,26 @@ document.addEventListener('DOMContentLoaded', () => {
     // if data is already cached, pre-fill the form
     if (cachedData) {
         document.getElementById('fullName').value = cachedData.fullName;
-        document.getElementById()
+        document.getElementById('contactNumber').value = cachedData.contactNumber;
+        document.getElementsById('homeArea').value = cachedData.homeArea;
+        document.getElementById('learningInstitution').value = cachedData.learningInstitution;
     }
-})
+
+    // event listener for submission of form
+    document.getElementById('customForm').addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const userData = {
+            fullName: document.getElementById('fullName').value,
+            contactNumber: document.getElementById('contactNumber').value,
+            homeArea: document.getElementsById('homeArea').value,
+            learningInstitution: document.getElementById('learningInstitution').value,
+        };
+
+        // store user data in local storage as user details
+        localStorage.setItem('userDetails', JSON.stringify(userData));
+
+        // Success alert
+        alert('Submitted. Be blessed!');
+    });
+});
